47fa1ea668568e2269d567834ddc9026c517c69e
Stickheadz32 <nkpit13moiv@realgymnasiet.se>
2016-01-17 00:06:23 +0100

    Feed Generator + XML Tools
    Testing for XML sitemaps.

--- musikpr/datetime.js
+++ musikpr/datetime.js
@@ -1,42 +1,36 @@
-function uT(){
+﻿function uT(){
 var _ = new Date(),
+
 	h = _.getHours(),
 	m = _.getMinutes(),
 	s = _.getSeconds(),
+
 	y = _.getFullYear(),
 	b = _.getDate(),
-	n = new Array();
 
-	//0:00:00 time format
+	/* Months */ /* IE5.5 gives warning about variable n */
+	n = ["January","February","March","April","May","June","July","August","September","October","November","December"],
+
+	k = b%10,
+	l = b%100;
+
+	/* 0:00:00 time format */
 	if (m < 10) m = "0"+m;
 	if (s < 10) s = "0"+s;
 
-	//Later browsers subtract getYear by 1900.
-	if(y<1900)y+1900;
-	
-	//Months
-	n[0]="January";
-	n[1]="February";
-	n[2]="March";
-	n[3]="April";
-	n[4]="May";
-	n[5]="June";
-	n[6]="July";
-	n[7]="August";
-	n[8]="September";
-	n[9]="October";
-	n[10]="November";
-	n[11]="December";
-	
-	//1st 2nd 3th 4th... 11th 12th 13th 14th... 21st 22nd 23rd 24th...
-	if (b%10 === 1 && b%100 !== 11 ) b = b+"st";
-	else if (b%10 === 2 && b%100 !== 12 ) b = b+"nd";
-	else if (b%10 === 3 && b%100 !== 13 ) b = b+"rd";
+	/* Later browsers subtract getYear by 1900. */
+	if (y < 1900) y += 1900;
+
+	/* 1st 2nd 3th 4th... 11th 12th 13th 14th... 21st 22nd 23rd 24th... */
+	if (k === 1 && l !== 11) b = b+"st";
+	else if (k === 2 && l !== 12) b = b+"nd";
+	else if (k === 3 && l !== 13) b = b+"rd";
 	else b = b+"th";
+
 	document.getElementById('dateNow_clock').innerHTML = h +":"+ m +":"+ s;
 	document.getElementById('dateNow_dayMonth').innerHTML = n[_.getMonth()] +" "+ b;
 	document.getElementById('dateNow_year').innerHTML = y;
 }
-//call function on load and repeat every 1000 ms
+/* call function on load and repeat every 1000 ms */
 uT();
 setInterval(uT, 1000);
---
+++ musikpr/feedgen/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {   /*NOW*/
+            DateTime a = DateTime.Now;
+            string[] m = { "jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "okt","nov","dec"};
+            string b = a.Year+"/"+m[a.Month-1]+"/"+a.Day.ToString();
+            textBox3.Text = b;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            resultURL.Text = "";
+            if (inputURL.Text != "")
+                if (inputURL.Text.Contains("http://")) resultURL.Text = inputURL.Text;
+                else
+            if (!inputURL.Text.Contains("http://") && !inputURL.Text.Contains("https://"))
+                resultURL.Text = "http://"+inputURL.Text;
+            if(inputURL.TextLength>7&&inputURL.Text[inputURL.TextLength-1]!='/')
+                resultURL.Text+="/";
+            if (textBox3.Text != "") resultURL.Text += textBox3.Text;
+            if (resultURL.Text!=""&&resultURL.Text[resultURL.TextLength - 1] != '/') resultURL.Text += "/";
+            if (textBox4.Text != "") resultURL.Text += textBox4.Text;
+            feedResult.Text="<!DOCTYPE html>\r\n";
+            feedResult.AppendText("<html>\r\n");
+            feedResult.AppendText("  <head>\r\n");
+            feedResult.AppendText("    <title>" + textBox1.Text == "" ? "-Untitled-" : textBox1.Text);
+            feedResult.Text+=inputSplit.Text[0]==' '?"":" ";
+            feedResult.AppendText("    <meta name=\"author\" content=\"" + textBox5.Text + "\"/>\r\n");
+            feedResult.AppendText("    <meta name=\"keywords\" content=\"" + textBox2.Text + "\"/>\r\n");
+            feedResult.AppendText("    <meta name=\"generator\" content=\"Stickheadz32's FeedGen v0.1\"/>\r\n");
+            feedResult.AppendText("  </head>\r\n\r\n");
+            feedResult.AppendText("  <body>\r\n");
+            feedResult.AppendText("    "+textBox6.Text+"\r\n");
+            feedResult.AppendText("  </body>\r\n");
+            feedResult.AppendText("</html>");
+        }
+    }
+}
