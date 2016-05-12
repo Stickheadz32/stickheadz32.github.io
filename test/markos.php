41a0c190a4fce32c11270c26f1074e8a0151cae6
Stickheadz32 <nkpit13moiv@realgymnasiet.se>
2016-05-12 13:42:28 +0200

    testing database functions v1

---
+++ test/markos.php
@@ -0,0 +1,50 @@
+﻿<html>
+
+<form action="markos.php" method="POST">
+	<input type="radio" value="Ja" name="svar">
+	<input type="radio" value="Nej" name="svar">
+	<input type="submit" value="submit">
+</form>
+
+<?php
+
+//Hämta svar från formen.
+$svar = $_POST["svar"];
+
+//Öppna fil i läsläge.
+$fil = fopen("markos.txt", "r") or die("Unable to open fil!");
+//Hämta filns innehåll.
+$innehall = fgets($fil);
+//Stäng filn.
+fclose($fil);
+
+//Dela på ja och nej.
+$kolumn = explode(",", $innehall);
+
+//Om svar är ja.
+if($svar == "Ja")
+{
+	//Öka ja svar med 1.
+	$kolumn[0]++;
+}
+//Annars
+else if($svar == "Nej")
+{
+	//Öka nej svar med 1.
+	$kolumn[1]++;
+}
+
+if( !empty($svar) );
+{
+	//Öppna fil i skrivläge.
+	$fil = fopen("markos.txt", "w") or die("Unable to open fil!");
+	fwrite($fil, $kolumn[0] . "," . $kolumn[1]);
+	fclose($fil);
+
+	//Skriv ut resultatet.
+	echo $kolumn[0] . "," . $kolumn[1];
+}
+
+?>
+
+</html>
